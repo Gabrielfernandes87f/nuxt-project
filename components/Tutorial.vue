215f9987a5d59e2d8738f006764a2bb09d5b6d05
Gabriel fernandes <Gabrielfernandesfotografias@outlook.com>
2022-03-14 12:32:17 -0300

    update projec Nuxt

---
+++ components/Tutorial.vue
@@ -0,0 +1,212 @@
+<!-- Please remove this file from your project -->
+<template>
+  <!-- This example requires Tailwind CSS v2.0+ -->
+  <div class="py-12 bg-orange-700">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="lg:text-center text-center sm:text-center">
+        <img
+          class="inline-block rounded-full object-cover leading-x w-32 h-32 sm:w-48 sm:h-48"
+          src="../assents/eu.jpg"
+          alt="avatar"
+        />
+        <h2
+          class="text-base text-indigo-600 font-semibold tracking-wide md:text-center sm:text-center"
+        >Engenheiro de software</h2>
+        <p
+          class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 md:text-center sm:text-center"
+        >Gabriel Fernandes</p>
+        <p
+          class="mt-4 leading-8 font-extrabold tracking-tight text-gray-500 md:text-center sm:text-center"
+        >Desenvolvedor fullstack/DevOps jr - Laravel</p>
+      </div>
+
+      <div class="overflow-hidden shadow-lg">
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">Sobre mim</div>
+          <p class="text-gray-700 text-base">
+            Desenvolvedor fullStack/DevOps
+            com mais de 2 ano de experiência.
+            Desenvolvimento de sistemas web, integração de sistemas.
+            Deploy de sistemas e web sites
+            Estudando e aprendendo novas tecnologias e ferramentas, todos os dias.
+            Foco em desenvolvimento de aplicação Laravel e Nuxt JS.
+          </p>
+        </div>
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">Experiência</div>
+          <p
+            class="text-gray-700 text-base">
+            loren ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+        </div>
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">Formação</div>
+          <p
+            class="text-gray-700 text-base"
+          >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+        </div>
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+          <p
+            class="text-gray-700 text-base"
+          >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #Laravel
+          </span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #Next
+          </span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #Nuxt
+          </span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #Git
+          </span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #Docker
+          </span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #Linux
+          </span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #JavaScript
+          </span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #tailwind
+          </span>
+
+        </div>
+      </div>
+
+      <div class="mt-10">
+        <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+          <div class="relative">
+            <dt>
+              <div
+                class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white"
+              >
+                <!-- Heroicon name: outline/globe-alt -->
+                <svg
+                  class="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                  />
+                </svg>
+              </div>
+              <p
+                class="ml-16 text-lg leading-6 font-medium text-gray-900"
+              >Competitive exchange rates</p>
+            </dt>
+            <dd
+              class="mt-2 ml-16 text-base text-gray-500"
+            >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+          </div>
+
+          <div class="relative">
+            <dt>
+              <div
+                class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white"
+              >
+                <!-- Heroicon name: outline/scale -->
+                <svg
+                  class="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
+                  />
+                </svg>
+              </div>
+              <p class="ml-16 text-lg leading-6 font-medium text-gray-900">No hidden fees</p>
+            </dt>
+            <dd
+              class="mt-2 ml-16 text-base text-gray-500"
+            >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+          </div>
+
+          <div class="relative">
+            <dt>
+              <div
+                class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white"
+              >
+                <!-- Heroicon name: outline/lightning-bolt -->
+                <svg
+                  class="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M13 10V3L4 14h7v7l9-11h-7z"
+                  />
+                </svg>
+              </div>
+              <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Transfers are instant</p>
+            </dt>
+            <dd
+              class="mt-2 ml-16 text-base text-gray-500"
+            >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+          </div>
+
+          <div class="relative">
+            <dt>
+              <div
+                class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white"
+              >
+                <!-- Heroicon name: outline/annotation -->
+                <svg
+                  class="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+                  />
+                </svg>
+              </div>
+              <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Mobile notifications</p>
+            </dt>
+            <dd
+              class="mt-2 ml-16 text-base text-gray-500"
+            >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
